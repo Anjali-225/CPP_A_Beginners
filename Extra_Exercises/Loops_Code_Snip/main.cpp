516eb11124f54798d4b6167b2069922ddd811525
Anjali <anjali.payal69@gmail.com>
2022-03-31 06:00:01 +0200

    Extra_Exercises

---
+++ Extra_Exercises/Loops_Code_Snip/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a = 0;
+    int b = 0;
+    int c = {};
+    int x = {};
+    int y = {};
+    int z = {};
+    
+    for (x {1}; x <= 10; x+=2) {
+        cout << a += 1 << endl;
+        
+        for (y=1; y <= 10; y++) {
+           cout << b += 1<< endl;
+           cout << c = a + b<< endl;
+           cout << z = x + y<< endl;
+        }
+    }
+    
+//    cout << "a = " << a << endl;
+//    cout << "b = " << b << endl;
+//    cout << "c = " << c << endl;
+//    cout << "x = " << x << endl;
+//    cout << "y = " << y << endl;
+//    cout << "z = " << z << endl;
+    
+    
+    
+//    std::cout << "My Template" << std::endl;
+    return 0;
+}
